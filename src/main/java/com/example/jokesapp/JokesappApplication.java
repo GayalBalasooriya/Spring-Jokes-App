8f15a57439e45c7eeba31d81884deccda2a586a2
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-17 19:51:45 +0530

    added services spring layer

--- src/main/java/com/example/jokesapp/JokesappApplication.java
+++ src/main/java/com/example/jokesapp/JokesappApplication.java
@@ -9,5 +9,7 @@ public class JokesappApplication {
     public static void main(String[] args) {
         SpringApplication.run(JokesappApplication.class, args);
     }
-
 }
+
+
+
---
+++ src/main/java/com/example/jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.example.jokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
