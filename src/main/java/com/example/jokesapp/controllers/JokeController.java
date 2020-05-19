0db94c03b79cc947e773d642fbbbbe6f498a7827
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-19 18:33:53 +0530

    added jokes app

---
+++ src/main/java/com/example/jokesapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.example.jokesapp.controllers;
+
+import com.example.jokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
